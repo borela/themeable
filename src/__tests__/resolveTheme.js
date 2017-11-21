51b2adb178ccf92171afe0a0cb58103ac30eb719
Alexandre Borela <alexandre.borela@gmail.com>
2017-11-21 04:48:01 -0200

    Change tests to the new resolveTheme signature

--- src/__tests__/resolveTheme.js
+++ src/__tests__/resolveTheme.js
@@ -10,11 +10,8 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
-import Adapter from 'enzyme-adapter-react-16'
-import React, { Component } from 'react'
-import { configure, shallow } from 'enzyme'
 import { CONTEXT_WITH_COMPONENT_THEME, CONTEXT_WITH_THEME } from 'data'
-import { resolveTheme, themeable } from '..'
+import { resolveTheme } from '..'
 
 import {
   CONTEXT as CONTEXT_COMPONENT_THEME,
@@ -26,37 +23,34 @@ import {
   NORMAL as NORMAL_THEME
 } from 'AwesomeTheme'
 
-configure({ adapter: new Adapter() })
-
 describe('Function “resolveTheme”', () => {
-  class NonThemeable {}
-
-  @themeable
-  class SomeComponentA extends Component {}
-
-  @themeable('custom-string')
-  class SomeComponentB extends Component {}
-
   it('returns “undefined” when an non themeable is passed', () => {
-    expect(resolveTheme(undefined)).toBeUndefined()
-    expect(resolveTheme(null)).toBeUndefined()
-    expect(resolveTheme(0)).toBeUndefined()
-    expect(resolveTheme('')).toBeUndefined()
-    expect(resolveTheme(NonThemeable)).toBeUndefined()
+    const NO_THEME_RESOLVED = {
+      componentTheme: undefined,
+      identifier: undefined,
+      source: undefined,
+      theme: undefined
+    }
+    expect(resolveTheme(undefined)).toEqual(NO_THEME_RESOLVED)
+    expect(resolveTheme(null)).toEqual(NO_THEME_RESOLVED)
+    expect(resolveTheme(0)).toEqual(NO_THEME_RESOLVED)
+    expect(resolveTheme('')).toEqual(NO_THEME_RESOLVED)
   })
 
   describe('when there’s', () => {
     describe('a “ComponentTheme” in the context', () => {
       it('returns the “ComponentTheme” from the context if the component has no identifier', () => {
-        const COMP_A1 = shallow(
-          <SomeComponentA/>,
-          CONTEXT_WITH_COMPONENT_THEME
-        ).instance()
+        const COMP_A1 = {
+          ...CONTEXT_WITH_COMPONENT_THEME,
+          identifier: undefined,
+          props: undefined
+        }
 
-        const COMP_A2 = shallow(
-          <SomeComponentA theme={NORMAL_THEME}/>,
-          CONTEXT_WITH_COMPONENT_THEME
-        ).instance()
+        const COMP_A2 = {
+          ...CONTEXT_WITH_COMPONENT_THEME,
+          identifier: undefined,
+          props: { theme: NORMAL_THEME }
+        }
 
         const RESOLVED = {
           componentTheme: CONTEXT_COMPONENT_THEME,
@@ -70,10 +64,11 @@ describe('Function “resolveTheme”', () => {
       })
 
       it('returns the “ComponentTheme” from the context if no theme is specified', () => {
-        const COMP = shallow(
-          <SomeComponentB/>,
-          CONTEXT_WITH_COMPONENT_THEME
-        ).instance()
+        const COMP = {
+          ...CONTEXT_WITH_COMPONENT_THEME,
+          identifier: 'custom-string',
+          props: undefined
+        }
 
         const RESOLVED = {
           componentTheme: CONTEXT_COMPONENT_THEME,
@@ -86,15 +81,17 @@ describe('Function “resolveTheme”', () => {
       })
 
       it('returns the specified “ComponentTheme”', () => {
-        const COMP_A = shallow(
-          <SomeComponentA theme={NORMAL_COMPONENT_THEME}/>,
-          CONTEXT_WITH_COMPONENT_THEME
-        ).instance()
+        const COMP_A = {
+          ...CONTEXT_WITH_COMPONENT_THEME,
+          identifier: undefined,
+          props: { theme: NORMAL_COMPONENT_THEME }
+        }
 
-        const COMP_B = shallow(
-          <SomeComponentB theme={NORMAL_COMPONENT_THEME}/>,
-          CONTEXT_WITH_COMPONENT_THEME
-        ).instance()
+        const COMP_B = {
+          ...CONTEXT_WITH_COMPONENT_THEME,
+          identifier: undefined,
+          props: { theme: NORMAL_COMPONENT_THEME }
+        }
 
         const RESOLVED = {
           componentTheme: NORMAL_COMPONENT_THEME,
@@ -108,10 +105,11 @@ describe('Function “resolveTheme”', () => {
       })
 
       it('returns the used “ComponentTheme” from the “Theme” specified', () => {
-        const COMP = shallow(
-          <SomeComponentB theme={NORMAL_THEME}/>,
-          CONTEXT_WITH_COMPONENT_THEME
-        ).instance()
+        const COMP = {
+          ...CONTEXT_WITH_COMPONENT_THEME,
+          identifier: 'custom-string',
+          props: { theme: NORMAL_THEME }
+        }
 
         const RESOLVED = {
           componentTheme: NORMAL_COMPONENT_THEME,
@@ -126,20 +124,36 @@ describe('Function “resolveTheme”', () => {
 
     describe('a “Theme” in the context', () => {
       it('returns “undefined” if the component has no identifier', () => {
-        const COMP_A1 = shallow(<SomeComponentA/>, CONTEXT_WITH_THEME).instance()
-        const COMP_A2 = shallow(<SomeComponentA theme={NORMAL_THEME}/>, CONTEXT_WITH_THEME).instance()
+        const COMP_A1 = {
+          ...CONTEXT_WITH_THEME,
+          identifier: undefined,
+          props: undefined
+        }
+
+        const COMP_A2 = {
+          ...CONTEXT_WITH_THEME,
+          identifier: undefined,
+          props: { theme: NORMAL_THEME }
+        }
+
         const RESOLVED = {
           componentTheme: undefined,
           identifier: undefined,
           source: undefined,
           theme: undefined
         }
+
         expect(resolveTheme(COMP_A1)).toEqual(RESOLVED)
         expect(resolveTheme(COMP_A2)).toEqual(RESOLVED)
       })
 
       it('returns the used “ComponentTheme” from the “Theme” loaded from the context', () => {
-        const COMP = shallow(<SomeComponentB/>, CONTEXT_WITH_THEME).instance()
+        const COMP = {
+          ...CONTEXT_WITH_THEME,
+          identifier: 'custom-string',
+          props: undefined
+        }
+
         const RESOLVED = {
           componentTheme: CONTEXT_COMPONENT_THEME,
           identifier: 'custom-string',
@@ -150,15 +164,17 @@ describe('Function “resolveTheme”', () => {
       })
 
       it('returns the “ComponentTheme” specified', () => {
-        const COMP_A = shallow(
-          <SomeComponentA theme={NORMAL_COMPONENT_THEME}/>,
-          CONTEXT_WITH_THEME
-        ).instance()
+        const COMP_A = {
+          ...CONTEXT_WITH_THEME,
+          identifier: undefined,
+          props: { theme: NORMAL_COMPONENT_THEME }
+        }
 
-        const COMP_B = shallow(
-          <SomeComponentB theme={NORMAL_COMPONENT_THEME}/>,
-          CONTEXT_WITH_THEME
-        ).instance()
+        const COMP_B = {
+          ...CONTEXT_WITH_THEME,
+          identifier: 'custom-string',
+          props: { theme: NORMAL_COMPONENT_THEME }
+        }
 
         const RESOLVED = {
           componentTheme: NORMAL_COMPONENT_THEME,
@@ -172,21 +188,37 @@ describe('Function “resolveTheme”', () => {
       })
 
       it('returns the used “ComponentTheme” from the “Theme” specified', () => {
-        const COMP = shallow(<SomeComponentB theme={NORMAL_THEME}/>, CONTEXT_WITH_THEME).instance()
+        const COMP = {
+          ...CONTEXT_WITH_THEME,
+          identifier: 'custom-string',
+          props: { theme: NORMAL_THEME }
+        }
+
         const RESOLVED = {
           componentTheme: NORMAL_COMPONENT_THEME,
           identifier: 'custom-string',
           source: 'property',
           theme: NORMAL_THEME
         }
+
         expect(resolveTheme(COMP)).toEqual(RESOLVED)
       })
     })
 
     describe('nothing in the context', () => {
       it('returns “undefined” if the component has no identifier', () => {
-        const COMP_A1 = shallow(<SomeComponentA/>).instance()
-        const COMP_A2 = shallow(<SomeComponentA theme={NORMAL_THEME}/>).instance()
+        const COMP_A1 = {
+          context: undefined,
+          identifier: undefined,
+          props: undefined
+        }
+
+        const COMP_A2 = {
+          context: undefined,
+          identifier: undefined,
+          props: { theme: NORMAL_THEME }
+        }
+
         const RESOLVED = {
           componentTheme: undefined,
           identifier: undefined,
@@ -198,26 +230,43 @@ describe('Function “resolveTheme”', () => {
       })
 
       it('returns the “ComponentTheme” specified', () => {
-        const COMP_A = shallow(<SomeComponentA theme={NORMAL_COMPONENT_THEME}/>).instance()
-        const COMP_B = shallow(<SomeComponentB theme={NORMAL_COMPONENT_THEME}/>).instance()
+        const COMP_A = {
+          context: undefined,
+          identifier: undefined,
+          props: { theme: NORMAL_COMPONENT_THEME }
+        }
+
+        const COMP_B = {
+          context: undefined,
+          identifier: 'custom-string',
+          props: { theme: NORMAL_COMPONENT_THEME }
+        }
+
         const RESOLVED = {
           componentTheme: NORMAL_COMPONENT_THEME,
           identifier: undefined,
           source: 'property',
           theme: undefined
         }
+
         expect(resolveTheme(COMP_A)).toEqual(RESOLVED)
         expect(resolveTheme(COMP_B)).toEqual(RESOLVED)
       })
 
       it('returns the used “ComponentTheme” from the “Theme” specified', () => {
-        const COMP = shallow(<SomeComponentB theme={NORMAL_THEME}/>).instance()
+        const COMP = {
+          context: undefined,
+          identifier: 'custom-string',
+          props: { theme: NORMAL_THEME }
+        }
+
         const RESOLVED = {
           componentTheme: NORMAL_COMPONENT_THEME,
           identifier: 'custom-string',
           source: 'property',
           theme: NORMAL_THEME
         }
+
         expect(resolveTheme(COMP)).toEqual(RESOLVED)
       })
     })

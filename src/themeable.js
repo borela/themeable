88cbb0d724c89752df7917758f0485678ab4de98
Alexandre Borela <alexandre.borela@gmail.com>
2017-11-27 10:19:14 -0200

    Disable some parts of the decorator

--- src/themeable.js
+++ src/themeable.js
@@ -10,21 +10,14 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
+// import resolveFlair from './resolveFlair'
+// import resolveTheme from './resolveTheme'
 import isThemeable from './isThemeable'
 import PropTypes from 'prop-types'
 import { Component } from 'react'
 import { isPresentable, presentable } from 'presentable'
 
-const SYMBOL = Symbol.for('themeable')
-const FLAIR_PATTERN = /^(\w+(?:\s+\w+)*)\s*!?/
-const PRESENTER_PATTERN = /!\s*(\w+)$/
-
-/**
- * Checks if an object has the format of a “Theme’, not a “ComponentTheme”.
- */
-function isTheme(target) {
-  return target && target.componentThemes !== undefined
-}
+export const SYMBOL = Symbol.for('themeable')
 
 function decorateComponent(identifier) {
   return targetComponent => {
@@ -56,133 +49,139 @@ function decorateComponent(identifier) {
       }
     })
 
-    prototype.getComponentTheme = function() {
-      let { theme = this.context.theme } = this.props
-      return isTheme(theme)
-        ? theme.componentThemes[this.getThemeableIdentifier()]
-        : theme
-    }
+    // prototype.getFlair = function() {
+    //   // No theme was specified, it is impossible to resolve the flair.
+    //   const COMPONENT_THEME = {} // this.getComponentTheme()
+    //   if (!COMPONENT_THEME)
+    //     return undefined
 
-    prototype.getFlair = function() {
-      // No theme was specified, it is impossible to resolve the flair.
-      const COMPONENT_THEME = this.getComponentTheme()
-      if (!COMPONENT_THEME)
-        return undefined
+    //   // Default or specific flair(s).
+    //   const { flair: FLAIR_PROP } = this.props
+    //   let flairs = COMPONENT_THEME.flairs.default
+    //   if (FLAIR_PROP) {
+    //     const MATCHES = FLAIR_PATTERN.exec(FLAIR_PROP)
+    //     if (MATCHES !== null)
+    //       flairs = MATCHES[1]
+    //   }
+    //   flairs = flairs.trim()
 
-      // Default or specific flair(s).
-      const { flair: FLAIR_PROP } = this.props
-      let flairs = COMPONENT_THEME.flairs.default
-      if (FLAIR_PROP) {
-        const MATCHES = FLAIR_PATTERN.exec(FLAIR_PROP)
-        if (MATCHES !== null)
-          flairs = MATCHES[1]
-      }
-      flairs = flairs.trim()
+    //   // Resolve the flair(s).
+    //   let result = ''
+    //   for (const TARGET of flairs.split(/\s+/)) {
+    //     let extracted = COMPONENT_THEME.flairs[TARGET]
 
-      // Resolve the flair(s).
-      let result = ''
-      for (const TARGET of flairs.split(/\s+/)) {
-        let extracted = COMPONENT_THEME.flairs[TARGET]
+    //     if (extracted !== undefined) {
+    //       result += ` ${Array.isArray(extracted) ? extracted.join(' ') : extracted}`
+    //       continue
+    //     }
 
-        if (extracted !== undefined) {
-          result += ` ${Array.isArray(extracted) ? extracted.join(' ') : extracted}`
-          continue
-        }
+    //     const THEME = this.getTheme()
+    //     const IDENTIFIER = this.getThemeableIdentifier()
+    //     const CLASS = prototype.constructor.name
 
-        const THEME = this.getTheme()
-        const IDENTIFIER = this.getThemeableIdentifier()
-        const CLASS = prototype.constructor.name
+    //     // Component without an identifier.
+    //     if (!IDENTIFIER)
+    //       throw new Error(`Flair “${TARGET}” is not defined for the component without identifier “${CLASS}”.`)
 
-        // Component without an identifier.
-        if (!IDENTIFIER)
-          throw new Error(`Flair “${TARGET}” is not defined for the component without identifier “${CLASS}”.`)
+    //     // Has an identifier but using a component theme.
+    //     if (!THEME)
+    //       throw new Error(`Flair “${TARGET}” is not defined for “${IDENTIFIER}” on the component theme.`)
 
-        // Has an identifier but using a component theme.
-        if (!THEME)
-          throw new Error(`Flair “${TARGET}” is not defined for “${IDENTIFIER}” on the component theme.`)
+    //     // Has an identifier and it is using a theme.
+    //     throw new Error(`Flair “${TARGET}” is not defined for “${IDENTIFIER}” on theme “${THEME.name}”.`)
+    //   }
 
-        // Has an identifier and it is using a theme.
-        throw new Error(`Flair “${TARGET}” is not defined for “${IDENTIFIER}” on theme “${THEME.name}”.`)
-      }
-
-      // Remove unnecessary spaces.
-      return result.replace(/\s+/g, ' ').trim()
-    }
+    //   // Remove unnecessary spaces.
+    //   return result.replace(/\s+/g, ' ').trim()
+    // }
 
     // Allows the presenter to be extracted from the “ComponentTheme” or “Theme”.
-    prototype.getPresenter = function() {
-      const COMPONENT_THEME = this.getComponentTheme()
-      if (!COMPONENT_THEME)
-        return this.props.presenter || this.defaultPresenter
+    // prototype.getPresenter = function() {
+    //   const COMPONENT_THEME = this.getComponentTheme()
+    //   if (!COMPONENT_THEME)
+    //     return this.props.presenter || this.defaultPresenter
 
-      // Specific presenter from the theme.
-      const { flair: FLAIR_PROP } = this.props
-      let presenter = COMPONENT_THEME.presenters.default
-      if (FLAIR_PROP) {
-        const MATCHES = PRESENTER_PATTERN.exec(FLAIR_PROP)
-        if (MATCHES !== null)
-          presenter = MATCHES[1]
-      }
+    //   // Specific presenter from the theme.
+    //   const { flair: FLAIR_PROP } = this.props
+    //   let presenter = COMPONENT_THEME.presenters.default
+    //   if (FLAIR_PROP) {
+    //     const MATCHES = PRESENTER_PATTERN.exec(FLAIR_PROP)
+    //     if (MATCHES !== null)
+    //       presenter = MATCHES[1]
+    //   }
 
-      let result = COMPONENT_THEME.presenters[presenter]
-      if (result !== undefined)
-        return result
+    //   let result = COMPONENT_THEME.presenters[presenter]
+    //   if (result !== undefined)
+    //     return result
 
-      const THEME = this.getTheme()
-      const IDENTIFIER = this.getThemeableIdentifier()
+    //   const THEME = this.getTheme()
+    //   const IDENTIFIER = this.getThemeableIdentifier()
 
-      // Using a component theme.
-      if (!THEME)
-        throw new Error(`Presenter “${presenter}” is not defined for “${IDENTIFIER}” on component theme.`)
+    //   // Using a component theme.
+    //   if (!THEME)
+    //     throw new Error(`Presenter “${presenter}” is not defined for “${IDENTIFIER}” on component theme.`)
 
-      // Using a theme.
-      const { name: THEME_NAME = '' } = this.getTheme()
-      throw new Error(`Presenter “${presenter}” is not defined for “${IDENTIFIER}” on theme “${THEME_NAME}”.`)
-    }
+    //   // Using a theme.
+    //   const { name: THEME_NAME = '' } = this.getTheme()
+    //   throw new Error(`Presenter “${presenter}” is not defined for “${IDENTIFIER}” on theme “${THEME_NAME}”.`)
+    // }
 
-    prototype.getTheme = function() {
-      const { theme: THEME } = this.props
-      if (!THEME)
-        return this.context.theme
-      return isTheme(THEME) ? THEME : undefined
-    }
+    // prototype.getTheme = function() {
+    //   const { theme: THEME } = this.props
+    //   if (!THEME)
+    //     return this.context.theme
+    //   return isTheme(THEME) ? THEME : undefined
+    // }
 
     // Used to combine the presentable’s data with the new attributes from the
     // themeable logic.
-    let oldGetPresentableData = prototype.getPresentableData
+    // let oldGetPresentableData = prototype.getPresentableData
 
     // The actual logic that combines the data.
-    prototype.getDefaultThemeableData = function() {
-      let result = oldGetPresentableData.call(this)
+    // prototype.getDefaultThemeableData = function() {
+    //   let result = oldGetPresentableData.call(this)
 
-      const RESOLVED_FLAIR = result.className
-        ? `${this.getFlair()} ${result.className}`
-        : this.getFlair()
+      // const IDENTIFIER = this.getThemeableIdentifier()
+      // const RESOLVED_THEME = resolveTheme({
+      //   context: result.context,
+      //   identifier: IDENTIFIER,
+      //   props: result.props
 
-      if (RESOLVED_FLAIR) {
-        if (result.props.className)
-          result.props.className += ` ${RESOLVED_FLAIR}`
-        else
-          result.props.className = RESOLVED_FLAIR
-      }
+      // })
 
-      delete result.props.flair
-      delete result.props.theme
-      return result
-    }
+      // if (!RESOLVED_THEME.componentTheme) {
+      //   delete result.context.theme
+      //   delete result.props.flair
+      //   delete result.props.theme
+      //   return
+      // }
+
+      // const RESOLVED_FLAIR = resolveFlair(RESOLVED_THEME, result.props.flair)
+    //   if (RESOLVED_FLAIR) {
+    //     if (result.props.className)
+    //       result.props.className += ` ${RESOLVED_FLAIR}`
+    //     else
+    //       result.props.className = RESOLVED_FLAIR
+    //   }
+
+    //   resolveFlair()
+    //   delete result.context.theme
+    //   delete result.props.flair
+    //   delete result.props.theme
+    //   return result
+    // }
+
+    // Add themeable hook if it doesn’t exist.
+    // if (!prototype.getThemeableData) {
+    //   prototype.getThemeableData = function() {
+    //     return getDefaultThemeableData()
+    //   }
+    // }
 
     // Implement the presentable data hook.
-    prototype.getPresentableData = function() {
-      return this.getThemeableData()
-    }
+    // prototype.getPresentableData = prototype.getThemeableData
 
-    // Add themeable hook.
-    if (!prototype.getThemeableData) {
-      prototype.getThemeableData = function() {
-        return this.getDefaultThemeableData()
-      }
-    }
-
+    // The final component with theming support.
     return targetComponent
   }
 }
@@ -195,7 +194,7 @@ function decorateComponent(identifier) {
  * the component to enable theming. If the component has no identifier, it’ll
  * only support themes passed directly to it.
  */
-export function themeable(identifierOrComponent:string|Class<Component>) {
+export function themeable(identifierOrComponent:string|Class<Component<*>>) {
   // It’s an identifier.
   if (typeof identifierOrComponent === 'string')
     return decorateComponent(identifierOrComponent)
